4cb4cc00ead17ad4761423b93a281c65b2a3dab6
Zangetsu001 <159882846+Zangetsu001@users.noreply.github.com>
2025-02-28 12:48:55 +0200

    Add files via upload

---
+++ Automobile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Automobile
+    {
+        string model;
+        int year;
+        public static int totalcars=0;
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+        public Automobile() { }
+        public Automobile(string model, int year)
+        {
+            this.Model = model;
+            this.Year = year;
+            totalcars++;
+        }
+       
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automobile auto1 = new Automobile("Жигули", 1999);
+            Automobile auto2 = new Automobile("Жигули", 2001);
+            Automobile auto3 = new Automobile("Жигули", 2005);
+            Automobile auto4 = new Automobile("Жигули", 2045);
+
+            Console.WriteLine($"Марка: {auto1.Model}\t Год Производства: {auto1.Year}");
+            Console.WriteLine($"Марка: {auto2.Model}\t Год Производства: {auto2.Year}");
+            Console.WriteLine($"Марка: {auto3.Model}\t Год Производства: {auto3.Year}");
+            Console.WriteLine($"Марка: {auto4.Model}\t Год Производства: {auto4.Year}");
+            Console.WriteLine(Automobile.totalcars);
+
+            Console.Read();
+        }
+    }
+}
